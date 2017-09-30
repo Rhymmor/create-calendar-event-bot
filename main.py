8d749761f432a98721e1b17d0229efbb94e07211
Anatoly Belonog <anatolybelonog@gmail.com>
2017-10-01 02:28:04 +0700

    add simple date parsing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import logging
-from telegram.ext import Updater, CommandHandler
+import parsedatetime as pdt
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
 
 def set_logging():
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -9,6 +10,18 @@ def set_logging():
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Hello, human!")
 
+def get_date(string):
+    cal = pdt.Calendar()
+    match = cal.nlp(string)
+    if match is not None and len(match) > 0:
+        return match[0][4]
+    else:
+        return None
+
+def parse_time(bot, update):
+    match = get_date(update.message.text)
+    if match is not None:
+        bot.send_message(chat_id=update.message.chat_id, text=match)
 
 def main():
     set_logging()
@@ -18,8 +31,17 @@ def main():
     bot_token = os.environ['BOT_TOKEN']
     updater = Updater(token=bot_token)
     dispatcher = updater.dispatcher 
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
-    updater.start_polling()
 
-main()
+    start_handler = CommandHandler('start', start)
+    message_handler = MessageHandler(Filters.text, parse_time)
+
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(message_handler)
+
+    updater.start_polling()
+    updater.idle()
+
+try:
+    main()
+except KeyboardInterrupt:
+    exit()
